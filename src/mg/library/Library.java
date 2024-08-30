c2c02df968221e0cf9efce4399c83595a4fb97ca
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-08-30 14:43:24 +0300

    minor changes

--- src/mg/library/Library.java
+++ src/mg/library/Library.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public abstract class Library {
+public class Library {
     private List<Book> books;
 
     public Library() {
